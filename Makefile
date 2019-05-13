126c6e9ab9f82f8f4d3b61d349ca977549dab46c
Don Park <donp@donp.org>
2019-05-13 16:23:40 -0700

    refresh attempt

--- Makefile
+++ Makefile
@@ -1,21 +1,13 @@
 export GOPATH=$(CURDIR)
-export CGO_CFLAGS=-I$(CURDIR)/leveldb-1.7.0/include
-export CGO_LDFLAGS=-L$(CURDIR)/leveldb-1.7.0
 
-all: leveldb-1.7.0/libleveldb.a src/code.google.com/p/goweb src/github.com/jmhodges/levigo boomerang60
+all: src/github.com/stretchr/goweb src/github.com/jmhodges/levigo boomerang
+
+boomerang: web.go
 	@echo building in $$GOPATH
 	go build
 
-src/code.google.com/p/goweb:
-	go get code.google.com/p/goweb
+src/github.com/stretchr/goweb:
+	go get github.com/stretchr/goweb
 
 src/github.com/jmhodges/levigo:
 	go get github.com/jmhodges/levigo
-
-leveldb-1.7.0/libleveldb.a: leveldb-1.7.0.tar.gz
-	$(MAKE) -C leveldb-1.7.0
-
-leveldb-1.7.0:
-	@echo downloading leveldb 1.7.0
-	wget http://leveldb.googlecode.com/files/leveldb-1.7.0.tar.gz	
-	tar zxf leveldb-1.7.0.tar.gz 
--- web.go
+++ web.go
@@ -3,7 +3,9 @@ package main
 import (
   "fmt"
   "bufio"
+  "net/http"
   "github.com/stretchr/goweb"
+  "github.com/stretchr/goweb/context"
   "github.com/jmhodges/levigo"
 )
 
@@ -15,37 +17,54 @@ func main() {
   wo := levigo.NewWriteOptions()
 
   db, err := levigo.Open("slugs.leveldb", opts)
-  if err == nil { }
-  goweb.ConfigureDefaultFormatters()
-  goweb.MapFunc("/{slug}", func(c *goweb.Context) {
-    
-    key := []byte(c.PathParams["slug"])
+  if err != nil { fmt.Printf("db %v\n", err) }
 
-    if c.IsGet() {
-      fmt.Printf("GET %s\n", c.PathParams["slug"])
-      data, err := db.Get(ro, key)
-      if err == nil { 
-        if data == nil {
-          c.WriteResponse("Not found", 404)
-        } else {
-         c.ResponseWriter.Header().Set("Location", string(data))
-         c.WriteResponse(data, 307)
-       }
+  goweb.Map("GET", "/{slug}", func(c context.Context) error {
+    key := c.PathValue("slug")
+    fmt.Printf("GET %s\n", key)
+    data, err := db.Get(ro, []byte(key))
+    if err == nil {
+      responseCode := 0
+      responseBytes := []byte("")
+      if data == nil {
+        responseBytes = []byte("Not found")
+        responseCode = 404
+      } else {
+        c.HttpResponseWriter().Header().Set("Location", string(data))
+        responseBytes = data
+        responseCode = 307
       }
+      return goweb.Respond.With(c, responseCode, responseBytes)
+    } else {
+      return goweb.Respond.With(c, http.StatusInternalServerError, nil)
     }
-
-    if c.IsPost() {
-      bodyio := bufio.NewReader(c.Request.Body)
-      body, err := bodyio.ReadString('\n')
-      if err == nil { }
-      
-      fmt.Printf("POST %s %s\n", key, body)
-      err = db.Put(wo, key, []byte(body))
-    }
-    
   })
 
-  fmt.Println("ready")
-  goweb.ListenAndServe(":8080")
+  goweb.Map("POST", "/{slug}", func(c context.Context) error {
+    key := c.PathValue("slug")
+
+    bodyio := bufio.NewReader(c.HttpRequest().Body)
+    body, err := bodyio.ReadString('\n')
+    if err == nil {
+      fmt.Printf("POST %s %s\n", key, body)
+      err = db.Put(wo, []byte(key), []byte(body))
+      fmt.Printf("POST2 %s %s\n", key, body)
+      if err == nil {
+        fmt.Printf("POST db good\n")
+        return goweb.Respond.With(c, 200, nil)
+      } else {
+        fmt.Printf("POST db %s %v\n", key, err)
+        return goweb.Respond.With(c, http.StatusInternalServerError, nil)
+      }
+    } else {
+      fmt.Printf("POST body err %v\n", key, err)
+      return goweb.Respond.With(c, http.StatusInternalServerError, nil)
+    }
+  })
+
+  addr := ":8080"
+  fmt.Printf("ready %s\n", addr)
+  s := &http.Server{Addr: addr, Handler: goweb.DefaultHttpHandler()}
+  s.ListenAndServe()
   fmt.Println("bye bye")
 }
